1f3f92a8f9df7b56d2434c7d33558968ffceffad
wontleave <wontleave@gmail.com>
2022-07-14 21:17:34 +0800

    Normal mode sampling for TS

--- testing_script.py
+++ testing_script.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import numpy as np
 import pyparsing as pp
-from ase import Atoms
+from ase import Atoms, units
+from ase.units import Bohr, Rydberg, kJ, kB, fs, Hartree, mol, kcal, second
+from scipy.stats import rv_discrete
 
+CM_2_AU = 4.5564e-6
+ANGS_2_AU = 1.8897259886
+AMU_2_AU = 1822.88985136
+k_B = 1.38064852e-23
+PLANCKS_CONS = 6.62607015e-34
 HA2J = 4.359744E-18
 BOHRS2ANG = 0.529177
 SPEEDOFLIGHT = 2.99792458E8
@@ -101,6 +108,7 @@ def moi_tensor(massvec, expmassvec, xyz):
 
     return xyz_com, moi_eigvec
 
+
 def trans_rot_vec(massvec, xyz_com, moi_eigvec):
 
     # Mass-weighted translational vectors
@@ -147,24 +155,99 @@ def trans_rot_vec(massvec, xyz_com, moi_eigvec):
     return dx_norms
 
 
-if __name__ == "__main__":
-    root_ = r"E:\TEST\Orca5Processor_tests\hessian_md\methylBr_F\run.AnHess.r2scan-3c.hess"
+class Boltzmann_gen(rv_discrete):
+    "Boltzmann distribution"
+    def _pmf(self, k, nu, temperature):
+         return ((np.exp(-(k * PLANCKS_CONS * nu)/(k_B * temperature))) *
+                (1 - np.exp(-(PLANCKS_CONS * nu)/(k_B * temperature))))
 
-    with open(root_) as f:
-        hessian_txt = f.read()
-    parsed = parse_hess_file(hessian_txt)
-    cart_hessian = make_sym_mat(parsed["hessian"])
-    atoms, massvec, coords3d = zip(*parsed["atoms"][2:])
-    coords3d = np.array([c.asList() for c in coords3d], dtype=float)
-    mol = Atoms(atoms, positions=coords3d)
-    massvec = np.array(massvec, dtype=float) * AMU2KG
+
+def reactive_normal_mode_sampling(xyz, force_constants_J_m_2,
+                                  proj_vib_freq_cm_1, proj_hessian_eigvec,
+                                  temperature,
+                                  kick=1):
+    """Normal Mode Sampling for Transition States. Takes in xyz(1,N,3), force_constants(3N-6) in J/m^2,
+    projected vibrational frequencies(3N-6) in cm^-1,mass-weighted projected hessian eigenvectors(3N-6,3N)
+    ,temperature in K, and scaling factor of initial velocity of the lowest imaginary mode.
+    Returns displaces xyz and a pair of velocities(forward and backwards)"""
+
+    # Determine the highest level occupany of each mode
+    occ_vib_modes = []
+    boltzmann = Boltzmann_gen(a=0, b=1000000, name="boltzmann")
+    for i, nu in enumerate(proj_vib_freq_cm_1):
+        if nu > 50:
+            occ_vib_modes.append(boltzmann.rvs(nu * SPEEDOFLIGHT * 100,
+                                               temperature))
+        elif i == 0:
+            occ_vib_modes.append(boltzmann.rvs(-1 * nu * SPEEDOFLIGHT * 100,
+                                               temperature))
+        else:
+            occ_vib_modes.append(-1)
+
+    # Determine maximum displacement (amplitude) of each mode
+
+    amplitudes = []
+    freqs = []
+    for i, occ in enumerate(occ_vib_modes):
+        if occ >= 0:
+            energy = proj_vib_freq_cm_1[i] * SPEEDOFLIGHT * 100 * PLANCKS_CONS  # cm-1 to Joules
+            amplitudes.append(np.sqrt((0.5 * (occ + 1) * energy) / force_constants_J_m_2[i]) * 1e9)  # Angstom
+        else:
+            amplitudes.append(0)
+
+    # Determine the actual displacements and velocities
+    displacements = []
+    velocities = []
+    random_0_1 = [np.random.normal(0, 1) for i in range(len(amplitudes))]
+    for i, amplitude in enumerate(amplitudes):
+
+        if force_constants_J_m_2[i] > 0:
+
+            displacements.append(amplitude
+                                 * np.cos(2 * np.pi * random_0_1[i])
+                                 * proj_hessian_eigvec[i])
+
+            velocities.append(-1 * proj_vib_freq_cm_1[i] * SPEEDOFLIGHT * 100 * 2 * np.pi
+                              * amplitude
+                              * np.sin(2 * np.pi * random_0_1[i])
+                              * proj_hessian_eigvec[i] / Bohr ** 2)
+
+        elif i == 0:
+
+            displacements.append(0)
+            velocities.append(0)
+
+            # Extra kick for lowest imagninary mode(s)
+            velocities.append(-1 * proj_vib_freq_cm_1[i] * SPEEDOFLIGHT * 100 * 2 * np.pi
+                              * amplitude
+                              * np.sin(2 * np.pi * random_0_1[i])
+                              * proj_hessian_eigvec[i] / Bohr ** 2)
+
+
+        else:
+
+            displacements.append(0)
+            velocities.append(0)
+
+    tot_disp = np.sum(np.array(displacements), axis=0)
+    # In angstroms
+    disp_xyz = xyz + tot_disp.reshape(1, -1, 3)
+    # In angstroms per second
+    tot_vel_plus = np.sum(np.array(velocities), axis=0).reshape(1, -1, 3)
+    tot_vel_minus = -1 * tot_vel_plus
+
+    return disp_xyz, tot_vel_plus, tot_vel_minus
+
+
+def vib_analysis(massvec_, cart_hessian, xyz_coords):
+
+    massvec = np.array(massvec_, dtype=float) * units._amu
     expmassvec = np.repeat(massvec, 3)
     sqrtinvmassvec = np.divide(1.0, np.sqrt(expmassvec))
     hessian_mwc = np.einsum('i,ij,j->ij', sqrtinvmassvec,
                             cart_hessian, sqrtinvmassvec)
-    hessian_eigval, hessian_eigvec = np.linalg.eig(hessian_mwc)
 
-    xyz_com, moi_eigvec = moi_tensor(massvec, expmassvec, coords3d)
+    xyz_com, moi_eigvec = moi_tensor(massvec, expmassvec, xyz_coords)
     dx_norms = trans_rot_vec(massvec, xyz_com, moi_eigvec)
 
     P = np.identity(3 * len(massvec))
@@ -174,10 +257,63 @@ if __name__ == "__main__":
     # Projecting the T and R modes out of the hessian
     mwhess_proj = np.dot(P.T, hessian_mwc).dot(P)
 
-    hessian_eigval, hessian_eigvec = np.linalg.eigh(mwhess_proj)
+    hessian_eigval, hessian_eigvec = np.linalg.eig(mwhess_proj)
+    neg_ele = []
+
+    for i, eigval in enumerate(hessian_eigval):
+        if eigval < 0:
+            neg_ele.append(i)
 
     hessian_eigval_abs = np.abs(hessian_eigval)
     pre_vib_freq_cm_1 = np.sqrt(
         hessian_eigval_abs * HA2J * 10e19) / (SPEEDOFLIGHT * 2 * np.pi * BOHRS2ANG * 100)
-    print()
+
+    vib_freq_cm_1 = pre_vib_freq_cm_1.copy()
+
+    for i in neg_ele:
+        vib_freq_cm_1[i] = -1 * pre_vib_freq_cm_1[i]
+
+    trans_rot_elms = []
+    for i, freq in enumerate(vib_freq_cm_1):
+        # Modes that are less than 1.0cm-1 is not a normal mode
+        if np.abs(freq) < 1.0:
+            trans_rot_elms.append(i)
+
+    force_constants_J_m_2 = np.delete(
+        hessian_eigval * HA2J * 1e20 / (BOHRS2ANG ** 2) * units._amu, trans_rot_elms)
+
+    proj_vib_freq_cm_1 = np.delete(vib_freq_cm_1, trans_rot_elms)
+    proj_hessian_eigvec = np.delete(hessian_eigvec.T, trans_rot_elms, 0)
+
+    print("PYTHON ---")
+    print(proj_vib_freq_cm_1)
+
+    print("ORCA ---")
+    print(parsed["vib_freqs"])
     # for folder in req_folders:
+    print(force_constants_J_m_2.astype(float))
+
+    return force_constants_J_m_2, proj_vib_freq_cm_1, proj_hessian_eigvec
+
+
+if __name__ == "__main__":
+    root_ = r"E:\TEST\Orca5Processor_tests\hessian_md\methylBr_F\run.AnHess.r2scan-3c.hess"
+
+    with open(root_) as f:
+        hessian_txt = f.read()
+    parsed = parse_hess_file(hessian_txt)
+    cartesian_hessian = make_sym_mat(parsed["hessian"])
+    atoms, mass, coords3d = zip(*parsed["atoms"][2:])
+    coords3d = np.array([c.asList() for c in coords3d], dtype=float)
+    
+    mol = Atoms(atoms, positions=coords3d)
+    fc_J_m_2, proj_freq_cm_1, proj_eigvec = vib_analysis(mass, cartesian_hessian, coords3d)
+
+    fc_J_m_2 = fc_J_m_2.astype(float)
+    displaced_xyz, total_velocities_plus, total_velocities_minus = reactive_normal_mode_sampling(coords3d, fc_J_m_2,
+                                                                                                 proj_freq_cm_1,
+                                                                                                 proj_eigvec,
+                                                                                                 temperature=298.15)
+    print(displaced_xyz)
+    print(total_velocities_plus)
+    print(total_velocities_minus)
